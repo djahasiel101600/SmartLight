f926ce26c04025f973cb1fecdf660fa5d37fe939
djahasiel101600 <djahasiel101600@gmail.com>
2026-05-17 11:55:34 +0800

    updated config

--- config.py
+++ config.py
@@ -128,6 +128,7 @@ PHOTORESISTOR_SETTLE_DELAY: float = 2
 # 4. System will use linear interpolation between calibration points
 # format: [raw adc]: [lux from meter]
 PHOTORESISTOR_CALIBRATION_POINTS: dict = {
+    120:22.9,
     215:110,
     408:160,
     591:1026
